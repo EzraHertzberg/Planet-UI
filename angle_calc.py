afe65171eb5392762737e4e58083ca4af5647e64
Ezra <ezrahertzberg42@gmail.com>
2025-11-06 22:50:39 -0500

    added timeset feature and moved planets closer together on y axis.

--- angle_calc.py
+++ angle_calc.py
@@ -1,17 +1,13 @@
 import math
 from skyfield.api import load
 from skyfield.framelib import ecliptic_frame
-planets = load('de421.bsp')  # ephemeris DE421
+planets = load('de430_1850-2150.bsp')  # ephemeris DE421
 
 def calc_angle(planet_id,t):
     position = planets[planet_id].at(t)
     x, y, z = position.frame_xyz(ecliptic_frame).au
     angle = (180/math.pi) * math.atan2(y, x)
-    print(x)
-    print(y)
-    print(angle)
-    
     return angle
 
 if __name__ == "__main__":
-    calc_angle(1)
+    pass
--- solar_system.py
+++ solar_system.py
@@ -19,11 +19,53 @@ commands = ["h","timeset","solarsystem"]
 grid = []
 
 def set_new_time():
+    year = 0
+    month = 0
+    day = 0
     while True:
-        year = input("Enter a date mm/dd/yyyy between or n for the current time: ")
-        if year == "n":
-            the_time = ts.now()
+        inp1 = input("Enter d to set date or n for the current time: ")
+        if inp1 == "n" or inp1 == "d":
             break
+        else:
+            print("Invalid input enter n or d")
+        
+    if inp1 == "n":
+        return ts.now()
+    else:
+        while True:
+            try:
+                year = int(input("Enter a year from 1850 to 2149: "))
+                if year > 1850 and year < 2149:
+                    break
+                else:
+                    print("Invalid year please enter year within range 1850-2149")
+            except TypeError:
+                print("please enter a number for the year")
+        while True:
+            try:
+                month = int(input("Enter a month from 1 to 12: "))
+                if month >= 1 and month <= 12:
+                    break
+                else:
+                    print("Invalid month enter int from 1 to 12")
+            except TypeError:
+                print("please enter a number for the month: ")
+        while True:
+            try:
+                day = int(input("Enter a day from 1 to 31: "))
+                if day < 1 or day > 31:
+                    print("invalid day enter day within range of 1 to 31")
+                elif month == 2 and (year % 4 != 0) and day > 28:
+                    print("invalid day, feburary is only 28 days long on non leap years")
+                elif month == 2 and (year % 4 == 0) and day > 29:
+                    print("invalid day, feburary is only 29 days long on leap years")
+                elif (month == 4 or month == 6 or month == 9 or month == 11) and day > 30:
+                        print("invalid day, this month is only 30 days long")
+                else:
+                    break
+            except TypeError:
+                print("please enter a number for the day")
+        return ts.utc(year, month, day, 0, 0, 0) 
         
         
 def grid_set():
@@ -71,13 +113,13 @@ class circle:
             if not hasattr(self, "ang"):
                 self.ang = init_ang * math.pi / 180
             self.x = round(origin.x + math.cos(self.ang) * 2 * dist)
-            self.y = round(origin.y - math.sin(self.ang) * dist)
+            self.y = round(origin.y - math.sin(self.ang) * 0.9 * dist)
             self.ang = self.ang + speed
 
 def solar_system():
     os.system("cls")
     grid_set()
-    sun = circle(80,25,4,"sun")
+    sun = circle(80,24,4,"sun")
     planets = ["place holder",
                circle(60,20,3, "mercury"),
                circle(60,20,3,"venus"),
@@ -92,7 +134,7 @@ def solar_system():
     sun.draw()
     for i, planet in enumerate(planets):
         if i > 0:
-            planet.orbit(sun, 6 + i * 3, angle_calc.calc_angle(i, the_time), 0) 
+            planet.orbit(sun, round(6 + i * 2.5), angle_calc.calc_angle(i, the_time), 0) 
             planet.draw()
             grid_call()
         
@@ -107,7 +149,9 @@ if __name__ == "__main__":
                 print("These are the valid commands: ")
                 for com in commands:
                     print(com)
-            if inp == "time_set":
-                set_new_time()
+            if inp == "timeset":
+                the_time = set_new_time()
+                os.system("cls")
+                print(f"time set to {the_time.utc_strftime()}")
         else:
             print("that's not a command")
